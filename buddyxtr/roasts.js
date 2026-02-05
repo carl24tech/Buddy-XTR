ccdd69ea03c83b8cd6a614536466538173f833d8
Carl William 🕵 <cwilly403@gmail.com>
2026-02-05 22:00:21 +0300

    @carl24tech
    Signed-off-by: Carl William 🕵  <cwilly403@gmail.com>

---
+++ buddyxtr/roasts.js
@@ -0,0 +1,15 @@
+// app/buddyxtr/roasts.js
+const roasts = [
+  "If laughter is the best medicine, your face must be curing the world.",
+  "You're the reason why aliens don't visit us.",
+  "I'd agree with you but then we'd both be wrong.",
+  "You're not stupid; you just have bad luck thinking.",
+  "Your IQ test came back negative.",
+  "You're like a cloud. When you disappear, it's a beautiful day.",
+  "Is your drama going to an intermission soon?",
+  "You bring everyone so much joy... when you leave the room.",
+  "I'd explain it to you but I left my English-to-Dumbass dictionary at home.",
+  "You have the perfect face for radio."
+];
+
+export default roasts;
